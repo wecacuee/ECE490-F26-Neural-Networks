17b4936e6903e6ace1e533546471e3f72fa579e4
Vikas Dhiman <wecacuee@users.noreply.github.com>
2026-02-20 12:35:22 -0500

    add OPEN in colab cell

--- scripts/export_ipynb_to_colab.py
+++ scripts/export_ipynb_to_colab.py
@@ -1,16 +1,34 @@
 import os
 import os.path as osp
 import nbformat
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
 
-def main(notebookpath, mathaliasesnotebook, exportpath):
+def yaml_load(config_yaml="myst.yml"):
+    with open(config_yaml) as cyf:
+        data = load(cyf, Loader=Loader)
+    return dict(repo_name=data['project']['github'])
+
+OPEN_IN_COLAB_STRING="<a href='https://colab.research.google.com/github/{repo_name}/blob/master/{file_path}'><span>OPEN IN COLAB</span></a> "
+
+def main(notebookpath, mathaliasesnotebook, exportpath, config_yaml):
+    variables = yaml_load(config_yaml)
+    variables.update(dict(file_path=notebookpath))
     with (open(notebookpath) as nbfp,
-          open(mathaliasesnotebook) as mafp,
-          open(exportpath, 'w') as exfp):
+          open(mathaliasesnotebook) as mafp):
         nb = nbformat.read(nbfp, 4)
         ma = nbformat.read(mafp, 4)
-        #print(nb['cells'][0])
-        nb['cells'][0] = ma['cells'][0]
-        print(f"Writing to '{exportpath}'")
+    #print(nb['cells'][0])
+    nb['cells'][0] = ma['cells'][0]
+    nb['cells'].insert(
+            1,
+            nbformat.v4.new_markdown_cell(
+                source=OPEN_IN_COLAB_STRING.format(**variables)))
+    print(f"Writing to '{exportpath}'")
+    with open(exportpath, 'w') as exfp:
         nbformat.write(nb, exfp)
 
 def ensuredirs(path):
@@ -29,7 +47,11 @@ if __name__ == '__main__':
     mathaliasesnotebook = osp.join(
             osp.dirname(__file__ or "."),
             "../templates/math-aliases.ipynb")
+    config_yaml  = osp.join(
+            osp.dirname(__file__ or "."),
+            "../myst.yml")
     main(sys.argv[1], 
          mathaliasesnotebook,
-         ensuredirs(colab_filepath))
+         ensuredirs(colab_filepath),
+         config_yaml)
 
